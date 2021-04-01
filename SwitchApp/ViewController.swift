4abd8203257eba233cbdda52833b22bffa1dced5
Alexey Barinov <begoodbebad2012@gmail.com>
2021-04-01 11:41:21 +0300

    added: SunView Switcher

--- SwitchApp/ViewController.swift
+++ SwitchApp/ViewController.swift
@@ -9,11 +9,28 @@ import UIKit
 
 class ViewController: UIViewController {
 
+    @IBOutlet var sunView: UIView!
+    @IBOutlet var switcher: UISwitch!
+    
     override func viewDidLoad() {
         super.viewDidLoad()
-        // Do any additional setup after loading the view.
+        
+        sunView.layer.cornerRadius = sunView.frame.size.height / 2
+        sunView.backgroundColor = .yellow
+        
+        switcher.tintColor = .clear
+        switcher.onTintColor = .gray
+        switcher.thumbTintColor = .yellow
     }
 
-
+    @IBAction func switchAction() {
+        
+        if switcher.isOn {
+            sunView.backgroundColor = .yellow
+        } else {
+            sunView.backgroundColor = .clear
+        }
+    }
+    
 }
 
